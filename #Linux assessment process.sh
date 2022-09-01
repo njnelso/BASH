e46a63702134e005a0b3bee755c74a800037fb70
Nick <32347189+njnelso@users.noreply.github.com>
2022-09-01 09:01:14 -0400

    Add files via upload

---
+++ #Linux assessment process.sh
@@ -0,0 +1,158 @@
+  #Linux assessment process
+  
+  235  mkdir copy
+  236  locate dnsmap.txt
+  237  cd /usr/share/wordlists/dnsmap.txt
+  238  cd /usr/share/wordlist
+  239  cd /usr/share/wordlists
+  240  ls
+  241  cp dnsmap.txt /tmp/copy/
+  242  cd /tmp
+  243  ls
+  244  cd copy
+  245  ls
+  246  cd ..
+  247  sudo wget https://shell.fullstackacademy.com/static/a632c1e05129e71d4ee85aa9b5e305f4/copy
+  248  ls
+  249  cd
+  250  sudo wget https://shell.fullstackacademy.com/static/a632c1e05129e71d4ee85aa9b5e305f4/copy
+  251  ls
+  252  chmod 777 copy
+  253  sudo chmod 777 copy
+  254  ./copy 
+  255  mkdir
+  256  mkdir test
+  257  cd test
+  258  echo "I will do great" > newFile
+  259  sudo wget https://shell.fullstackacademy.com/static/b9bc08ad0518d91617ccb042301b7cd6/create
+  260  ls
+  261  sudo chmod 777 create
+  262  ls
+  263  ./create
+  264  sudo wget https://shell.fullstackacademy.com/static/4a0403b777958c3278f60bc5a888dbfb/delete.zip
+  265  unzip delete.zip 
+  266  ls
+  267  cd delete
+  268  ls
+  269  rm -r 'A*
+
+  270  rm -r 'A*'
+  271  man rm
+  272  rm "A*"
+  273  rm "*3"
+  274  ls
+  275  rm *3
+  276  rm A*
+  277  rm *F*
+  278  ls
+  279  cd ..
+  280  sudo wget https://shell.fullstackacademy.com/static/4a0403b777958c3278f60bc5a888dbfb/delete-checker
+  281  ls
+  282  sudo chmod 777 delete-checker 
+  283  ./delete-checker 
+  284  sudo wget https://shell.fullstackacademy.com/static/38c441870d14a8191607f895ec3b1874/display.tar.gz
+  285  tar -xvf display.tar.gz 
+  286  ls
+  287  cat display
+  288  cd display
+  289  ls
+  290  find . -name "FS{"
+  291  grep -r "FS{"
+  292  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" | xxd -r | base64 -d
+  293  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" | xxd -r | base64 -d > encode
+  294  ls
+  295  cat encode
+  296  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" | xxd -r
+  297  cd ..
+  298  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" > encode
+  299  xxd -r encode
+  300  cat encode
+  301  xxd -r encode > encode
+  302* 
+  303  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" | xxd -r -p
+  304  echo "526c4e375a57356a62325270626d646651554645526a4242525452454d7a637a52454d334f48303d" | xxd -r -p | base64 -d
+  305  adduser
+  306  sudo adduser new-user
+  307  sudo wget https://shell.fullstackacademy.com/static/2a6835a418bf5d932886b9c7095b62ae/new-user
+  308  ls
+  309  sudo chmod 777 new-user
+  310  ./new-user
+  311  cd /tmp
+  312  touch permissions
+  313  sudo chown permissions www-data
+  314  sudo chown www-data permissions 
+  315  sudo chgrp www-data permissions
+  316  ls -l permissions
+  317  sudo chmod 635 permissions
+  318  ls -l permissions
+  319  sudo wget https://shell.fullstackacademy.com/static/96e35d94978f8056db4124e35e0cc6a2/permissions
+  320  ls
+  321  sudo chmod 777 permissions.1
+  322  ./permissions.1
+  323  sudo wget https://shell.fullstackacademy.com/static/22afc820da796411c127a57178a2fce0/timer.sh
+  324  ls
+  325  sudo chmod 777 timer.sh 
+  326  sudo wget https://shell.fullstackacademy.com/static/22afc820da796411c127a57178a2fce0/scheduling
+  327  sudo chmod 777 scheduling 
+  328  crontab -e
+  329  ./scheduling 
+  330  ./timer.sh
+  331  ls
+  332  crontab -e
+  333  ./scheduling 
+  334  mv scheduling ~/test
+  335  sudo mv scheduling ~/test
+  336  cd
+  337  cd test
+  338  ./scheduling 
+  339  cd /tmp
+  340  rm timer.log
+  341  ls
+  342  cron
+  343  sudo cron
+  344  crontab -e
+  345  cd ~/test
+  346  ./scheduling 
+  347  crontab -e
+  348  sudo mv scheduling /tmp/scheduling
+  349  ls
+  350  cd /tmp
+  351  ls
+  352  sudo ./scheduling 
+  353  nano timer.sh
+  354  rm timer.sj
+  355  rm timer.sh
+  356  sudo rm timer.sh
+  357  ls
+  358  sudo wget https://shell.fullstackacademy.com/static/22afc820da796411c127a57178a2fce0/timer.sh
+  359  ls
+  360  ./scheduling
+  361  sudo chmod timer.sh
+  362  sudo chmod 777 timer.sh
+  363  ./scheduling 
+  364  nano scheduling 
+  365  crontab -r
+  366  crontab -e
+  367  ./scheduling
+  368  cd ~/test
+  369  sudo wget https://shell.fullstackacademy.com/static/b33a8356eae8cdb91a5f555ea094f83c/search.zip
+  370  unzip search.zip 
+  371  ls
+  372  cd searcg
+  373  cd search/
+  374  ls
+  375  find -size 1184c
+  376  cd search14
+  377  cat search2.txt
+  378  cd
+  379  sudo updatedb
+  380  locate directory-list-2.3-medium.txt
+  381  cd /usr/share/wordlists/dirbuster
+  382  ls
+  383  grep "*www*"
+  384  grep www directory-list-2.3-medium.txt | sort | head -20 > /tmp/sort-answer.txt
+  385  cat /tmp/sort-answer.txt
+  386  cd ~/test
+  387  sudo wget https://shell.fullstackacademy.com/static/16c1db78595c62adca43dc12067d2571/sort
+  388  sudo chmod 777 sort
+  389  ./sort
